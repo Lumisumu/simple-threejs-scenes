1c6939a2d34a384d2a08bc500707b764b612d4fc
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-02-04 20:08:18 +0200

    Add first scene

---
+++ scenes/animated-object.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+const clock = new THREE.Clock();
+let mixer = new THREE.AnimationMixer();
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Camera and lights
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const directionalLight = new THREE.DirectionalLight(0xfff, 1);
+const ambientLight = new THREE.AmbientLight(0xfff, 1);
+scene.add(directionalLight, ambientLight);
+
+//Load model and play animation
+loader.load("/model.glb", function (gltf) {
+  scene.add(gltf.scene);
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  const action = mixer.clipAction(
+    THREE.AnimationClip.findByName(gltf.animations, "animation1")
+  );
+  action.play();
+});
+
+//Render loop
+function animate() {
+  const delta = clock.getDelta();
+  mixer.update(delta);
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+animate();
